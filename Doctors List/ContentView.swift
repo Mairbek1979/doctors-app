4076ffbeb206214dc77937d44d4f013a65b648fb
Mairbek <gmt271279@gmail.com>
2025-11-04 14:27:32 +0300

    Initial Commit

---
+++ Doctors List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Doctors List
+//
+//  Created by Mairbek Gairbekov on 4.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Doctors List/Doctors_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doctors_ListApp.swift
+//  Doctors List
+//
+//  Created by Mairbek Gairbekov on 4.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Doctors_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
